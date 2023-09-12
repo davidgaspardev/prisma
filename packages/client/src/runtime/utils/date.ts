6a487e25a49efda255b0a14f19ff715ca3822ab3
David Gaspar <davidgaspar.dev@gmail.com>
2023-09-12 15:28:10 -0300

    chore(utils): format date object to date time string

--- packages/client/src/runtime/utils/date.ts
+++ packages/client/src/runtime/utils/date.ts
@@ -9,3 +9,22 @@ export function isDate(value: unknown): value is Date {
 export function isValidDate(date: Date) {
   return date.toString() !== 'Invalid Date'
 }
+
+export function formatDateToDateTime(date: Date) {
+  const year = date.getFullYear();
+  const month = setDigits(date.getMonth() + 1);
+  const day = setDigits(date.getDate());
+  const hours = setDigits(date.getHours());
+  const minutes = setDigits(date.getMinutes());
+  const seconds = setDigits(date.getSeconds());
+  const milliseconds = setDigits(date.getMilliseconds(), 3);
+
+  const dateFormat = `${year}-${month}-${day}`;
+  const timeFormat = `${hours}:${minutes}:${seconds}.${milliseconds}`;
+
+  return `${dateFormat} ${timeFormat}`;
+}
+
+function setDigits(num: number, len: number = 2) {
+  return num.toString().padStart(len, "0");
+}
